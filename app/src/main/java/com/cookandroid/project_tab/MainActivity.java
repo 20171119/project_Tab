5b3cddd311372dd04cd9a0ae18a99127a38e9295
jspark98 <jspark98@unist.ac.kr>
2021-06-30 21:14:18 +0900

    success call

--- app/src/main/java/com/cookandroid/project_tab/MainActivity.java
+++ app/src/main/java/com/cookandroid/project_tab/MainActivity.java
@@ -2,13 +2,133 @@ package com.cookandroid.project_tab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.view.View.OnClickListener;
+import android.app.ProgressDialog;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    LinearLayout ll;
+//    Button loadBtn;
+
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup() ;
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator("Call") ;
+        tabHost1.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator("Gallery") ;
+        tabHost1.addTab(ts2) ;
+
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3") ;
+        ts3.setContent(R.id.content3) ;
+        ts3.setIndicator("TAB 3") ;
+        tabHost1.addTab(ts3) ;
+
+        ll = (LinearLayout) findViewById(R.id.content1);
+
+        list = (ListView) findViewById(R.id.listView1);
+
+        LoadContactsAyscn lca = new LoadContactsAyscn();
+        lca.execute();
+
+//        loadBtn = (Button) findViewById(R.id.button1);
+//        loadBtn.setOnClickListener(new OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//                System.out.println("OnClick");
+//                LoadContactsAyscn lca = new LoadContactsAyscn();
+//                lca.execute();
+//            }
+//        });
     }
+
+    class LoadContactsAyscn extends AsyncTask<Void, Void, ArrayList<String>> {
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+
+            pd = ProgressDialog.show(MainActivity.this, "Loading Contacts",
+                    "Please Wait");
+        }
+
+        @Override
+        protected ArrayList<String> doInBackground(Void... params) {
+            // TODO Auto-generated method stub
+            ArrayList<String> contacts = new ArrayList<String>();
+
+            Cursor c = getContentResolver().query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                    null, null, null);
+            while (c.moveToNext()) {
+
+                String contactName = c
+                        .getString(c
+                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phNumber = c
+                        .getString(c
+                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                contacts.add(contactName + ":" + phNumber);
+
+            }
+            Collections.sort(contacts);
+            c.close();
+
+            return contacts;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> contacts) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(contacts);
+
+            pd.cancel();
+
+//            ll.removeView(loadBtn);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    getApplicationContext(), R.layout.text, contacts);
+
+            list.setAdapter(adapter);
+
+        }
+
+    }
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        pd.dismiss();
+//    }
 }
