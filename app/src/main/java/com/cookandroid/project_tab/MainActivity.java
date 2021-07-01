a48fa55dd4085485e7ebc6215869fb9fdbe48242
jspark98 <jspark98@unist.ac.kr>
2021-07-01 13:22:21 +0900

    success gallery version1

--- app/src/main/java/com/cookandroid/project_tab/MainActivity.java
+++ app/src/main/java/com/cookandroid/project_tab/MainActivity.java
@@ -44,25 +44,6 @@ public class MainActivity extends AppCompatActivity {
     private final int GET_GALLERY_IMAGE = 200;
     private ImageView imageview;
 
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
-    public int inSampleSize = 1;
-
-    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-    private Uri fileUri;
-    private static String basePath;
-
-    public float imageViewRotation = 90;
-    public String TAG = "Camera Example :: ";
-
-    private Button takePicBtn;
-    private ImageView resultView;
-    private TextView imgPath;
-    private Gallery customGallery;
-    private CustomGalleryAdapter customGalAdapter;
-
-    private String[] imgs;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,10 +70,6 @@ public class MainActivity extends AppCompatActivity {
         ts3.setIndicator("TAB 3") ;
         tabHost1.addTab(ts3) ;
 
-        TabHost.TabSpec ts4 = tabHost1.newTabSpec("Tab Spec 4") ;
-        ts3.setContent(R.id.content4) ;
-        ts3.setIndicator("TAB 4") ;
-        tabHost1.addTab(ts4) ;
 
         // Call 변수 미리 설정
         ll = (LinearLayout) findViewById(R.id.content1);
@@ -107,76 +84,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
                 startActivityForResult(intent, GET_GALLERY_IMAGE);
-            }
-        });
-
-        // App.을 실행하자 마자 지정한 경로의 생성 및 접근에 용이하도록 아래와 같이 생성
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (! mediaStorageDir.exists()){
-            if (! mediaStorageDir.mkdirs()){
-                Log.d("MyCameraApp", "failed to create directory");
-//                return null;
-            }
-        }
-
-        basePath = mediaStorageDir.getPath();
-
-        imgPath = (TextView)findViewById(R.id.imgpath);
-        resultView = (ImageView)findViewById(R.id.resultview);
-        takePicBtn = (Button)findViewById(R.id.takepicbtn);
-        // Button click시, Camera Intent를 불러 옴
-        takePicBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // create Intent to take a picture and return control to the calling application
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-//                fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-//                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
-//
-//                // start the image capture Intent
-//                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-                File imagesFolder = new File(Environment.getExternalStorageDirectory(), "MyImages");
-                imagesFolder.mkdirs(); // <----
-                File image = new File(imagesFolder, "image_001.jpg");
-                Uri uriSavedImage = Uri.fromFile(image);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
-                startActivityForResult(intent,0);
-            }
-        });
-
-        File file = new File(basePath);
-        imgs = file.list();
-        for(int i=0; i<imgs.length; i++){
-            imgPath.setText(imgs[i]);
-        }
-
-        customGallery = (Gallery)findViewById(R.id.customgallery); // activity_main.xml에서 선언한 Gallery를 연결
-        customGalAdapter = new CustomGalleryAdapter(getApplicationContext(), basePath); // 위 Gallery에 대한 Adapter를 선언
-        customGallery.setAdapter(customGalAdapter); // Gallery에 위 Adapter를 연결
-        // Gallery의 Item을 Click할 경우 ImageView에 보여주도록 함
-        customGallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Bitmap bm = BitmapFactory.decodeFile(basePath+ File.separator +imgs[position]);
-                Bitmap bm2 = ThumbnailUtils.extractThumbnail(bm, bm.getWidth() / inSampleSize, bm.getHeight() / inSampleSize);
-                resultView.setImageBitmap(bm2);
-                imgPath.setText(basePath+File.separator+imgs[position]);
-            }
-        });
-        // Gallery의 Item을 LongClick할 경우 해당 File을 삭제하도록 함.
-        // 이 부분에서 동작은 하나, 삭제 후 결과가 View에 반영이 안되어 추가 보완 필요
-        customGallery.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                File temp = new File(basePath+File.separator+imgs[position]);
-                temp.delete();
-                return false;
             }
         });
     }
