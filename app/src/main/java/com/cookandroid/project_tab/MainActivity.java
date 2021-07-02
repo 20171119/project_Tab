7ca332d49c6e37797d89912722ee002f63fbc7af
jspark98 <jspark98@unist.ac.kr>
2021-07-02 11:44:12 +0900

    before design call

--- app/src/main/java/com/cookandroid/project_tab/MainActivity.java
+++ app/src/main/java/com/cookandroid/project_tab/MainActivity.java
@@ -16,11 +16,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TabHost;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import android.net.Uri;
 import android.widget.ImageView;
@@ -396,7 +399,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    class LoadContactsAyscn extends AsyncTask<Void, Void, ArrayList<String>> {
+    class LoadContactsAyscn extends AsyncTask<Void, Void, ArrayList<Map<String,String>>> {
 
         @Override
         protected void onPreExecute() {
@@ -408,40 +411,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected ArrayList<String> doInBackground(Void... params) {
+        protected ArrayList<Map<String, String>> doInBackground(Void... params) {
             // TODO Auto-generated method stub
-            ArrayList<String> contacts = new ArrayList<String>();
+            ArrayList<Map<String, String>> contacts = new ArrayList<Map<String, String>>();
 
             Cursor c = getContentResolver().query(
-                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                    null, null, null);
+                    ContactsContract.Contacts.CONTENT_URI, null,
+                    null, null, ContactsContract.Contacts.DISPLAY_NAME_PRIMARY+" asc");
+
             while (c.moveToNext()) {
+                HashMap<String, String> map = new HashMap<String, String>();
 
-                String contactName = c
-                        .getString(c
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String phNumber = c
-                        .getString(c
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                //연락처 id값
+                String id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
 
-                contacts.add(contactName + ":" + phNumber);
+                //연락처 대표이름
+                String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
 
-            }
-            Collections.sort(contacts);
+                map.put("name", name);
+
+                //id로 전화 정보 조회
+                Cursor phoneCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+
+                //데이터가 있는 경우
+                if(phoneCursor.moveToFirst()){
+                    String number = phoneCursor.getString(phoneCursor.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    map.put("phone", number);
+                }
+                phoneCursor.close();
+                contacts.add(map);
+
+            } //end while
+
             c.close();
 
             return contacts;
         }
 
         @Override
-        protected void onPostExecute(ArrayList<String> contacts) {
+        protected void onPostExecute(ArrayList<Map<String, String>> contacts) {
             // TODO Auto-generated method stub
             super.onPostExecute(contacts);
 
             pd.cancel();
 
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                    getApplicationContext(), R.layout.text, contacts);
+            SimpleAdapter adapter = new SimpleAdapter(
+                    getApplicationContext(), contacts, android.R.layout.simple_list_item_2,
+                    new String[]{"name","phone"}, new int[]{android.R.id.text1, android.R.id.text2});
 
             list.setAdapter(adapter);
         }
